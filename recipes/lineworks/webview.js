93ac3bb2ecb5ca9fe4efd2524c622a069e2b4083
Hiroshi Miura <miurahr@linux.com>
2023-03-25 20:53:34 +0900

    get message count

--- recipes/lineworks/webview.js
+++ recipes/lineworks/webview.js
@@ -1,1 +1,15 @@
-module.exports = Ferdium => Ferdium;
+module.exports = Ferdium => {
+  const getMessages = () => {
+    const elements = document.querySelectorAll(
+      'div#chat_list > ul#chat_grp_lst > li#item_chat > dl#chat_count > span#new',
+    );
+    let count = 0;
+
+    if (elements[0]) {
+      count = Ferdium.safeParseInt(elements[0].textContent);
+    }
+
+    Ferdium.setBadge(count);
+  };
+  Ferdium.loop(getMessages);
+}
